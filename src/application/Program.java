03cb06fa9c2a14ffee154f4b766a082d83351a0c
Michelle Madeira <michellemadeira1@gmail.com>
2022-11-22 18:05:01 -0300

    Demo-deletar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -16,20 +17,18 @@ public class Program {
 			conn = DB.getConnection();
 	
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
-					+ "WHERE "
-					+ "(DepartmentId = ?)");
-
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+					"DELETE FROM department "
+					+"WHERE"
+					+"Id = ?");
+					
+			st.setInt(1, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		} 
 		finally {
 			DB.closeStatement(st);
